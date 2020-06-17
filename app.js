d26921160fbe87dd0083788dc1d517f654192851
Smit <Smit>
2020-06-17 13:07:03 +0530

    version 1.0

--- app.js
+++ app.js
@@ -54,6 +54,7 @@ app.get("/about",function(req,res){
 
 
 
-app.listen(3000,function(){
-	console.log("Server started on port 3000");
-})
+app.listen(process.env.PORT || 3000,function(){
+	console.log("Server is running on port 3000");
+
+}) 
